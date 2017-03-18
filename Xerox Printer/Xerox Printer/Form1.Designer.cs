62d95e57a65d6f20ecf3e3e7fc6f8f406065f919
Clement Gamache <clementgamache@gmail.com>
2017-03-17 21:53:22 -0400

    put my work in the git repo

---
+++ Xerox Printer/Xerox Printer/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Xerox_Printer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelSelectedFile = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.buttonSelectFile = new System.Windows.Forms.Button();
+            this.buttonPrint = new System.Windows.Forms.Button();
+            this.printDocument = new System.Drawing.Printing.PrintDocument();
+            this.SuspendLayout();
+            // 
+            // labelSelectedFile
+            // 
+            this.labelSelectedFile.AutoSize = true;
+            this.labelSelectedFile.Location = new System.Drawing.Point(12, 77);
+            this.labelSelectedFile.Name = "labelSelectedFile";
+            this.labelSelectedFile.Size = new System.Drawing.Size(152, 20);
+            this.labelSelectedFile.TabIndex = 0;
+            this.labelSelectedFile.Text = "Please select a file...";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // buttonSelectFile
+            // 
+            this.buttonSelectFile.Location = new System.Drawing.Point(12, 12);
+            this.buttonSelectFile.Name = "buttonSelectFile";
+            this.buttonSelectFile.Size = new System.Drawing.Size(208, 55);
+            this.buttonSelectFile.TabIndex = 1;
+            this.buttonSelectFile.Text = "Select file...";
+            this.buttonSelectFile.UseVisualStyleBackColor = true;
+            this.buttonSelectFile.Click += new System.EventHandler(this.buttonSelectFile_Click);
+            // 
+            // buttonPrint
+            // 
+            this.buttonPrint.Location = new System.Drawing.Point(12, 147);
+            this.buttonPrint.Name = "buttonPrint";
+            this.buttonPrint.Size = new System.Drawing.Size(544, 74);
+            this.buttonPrint.TabIndex = 2;
+            this.buttonPrint.Text = "Print";
+            this.buttonPrint.UseVisualStyleBackColor = true;
+            this.buttonPrint.Click += new System.EventHandler(this.buttonPrint_Click);
+            // 
+            // printDocument
+            // 
+            this.printDocument.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument_PrintPage);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 244);
+            this.Controls.Add(this.buttonPrint);
+            this.Controls.Add(this.buttonSelectFile);
+            this.Controls.Add(this.labelSelectedFile);
+            this.Name = "Form1";
+            this.Text = "Xerox Printer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelSelectedFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button buttonSelectFile;
+        private System.Windows.Forms.Button buttonPrint;
+        private System.Drawing.Printing.PrintDocument printDocument;
+    }
+}
+
---
+++ Xerox Printer/Xerox Printer/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Xerox_Printer
+{
+    public partial class Form1 : Form
+    {
+        public static Image ScaleImage(Image image, int maxWidth, int maxHeight)
+        {
+            var ratioX = (double)maxWidth / image.Width;
+            var ratioY = (double)maxHeight / image.Height;
+            var ratio = Math.Min(ratioX, ratioY);
+
+            var newWidth = (int)(image.Width * ratio);
+            var newHeight = (int)(image.Height * ratio);
+
+            var newImage = new Bitmap(newWidth, newHeight);
+
+            using (var graphics = Graphics.FromImage(newImage))
+                graphics.DrawImage(image, 0, 0, newWidth, newHeight);
+
+            return newImage;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSelectFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog.ShowDialog();
+            labelSelectedFile.Text = openFileDialog.FileName;
+        }
+
+        private void buttonPrint_Click(object sender, EventArgs e)
+        {
+            System.Drawing.Image img = System.Drawing.Image.FromFile(openFileDialog.FileName);
+            //MessageBox.Show(
+            //    printDocument.DefaultPageSettings.Margins.Bottom.ToString() + "\n" +
+            //    printDocument.DefaultPageSettings.Margins.Bottom.ToString() + "\n" +
+            //    printDocument.DefaultPageSettings.Margins.Bottom.ToString() + "\n" +
+            //    printDocument.DefaultPageSettings.Margins.Bottom.ToString() + "\n");
+            printDocument.DefaultPageSettings.Margins = new System.Drawing.Printing.Margins(30,30,30,30);
+            //printDocument.DefaultPageSettings.PaperSource = printDocument.PrinterSettings.PaperSources[0];
+            printDocument.DefaultPageSettings.PaperSize = printDocument.PrinterSettings.PaperSizes[11];
+            MessageBox.Show(printDocument.DefaultPageSettings.PaperSize.ToString());
+            MessageBox.Show(printDocument.DefaultPageSettings.PaperSource.ToString());
+            //printDocument.DefaultPageSettings.PaperSize = new System.Drawing.Printing.PaperSize("12x18", 1200, 1800);//printDocument.PrinterSettings.PaperSizes[11];
+            //printDocument.DefaultPageSettings.PrinterSettings.
+            //MessageBox.Show(printDocument.PrinterSettings.PaperSizes[11].ToString());
+            //for (int i = 0; i < printDocument.PrinterSettings.PaperSources.Count; i++)
+            //{
+            //    MessageBox.Show(printDocument.PrinterSettings.PaperSources[i].ToString());
+            //}
+            if (img.Width > img.Height)
+            {
+                printDocument.DefaultPageSettings.Landscape = true;
+            }
+            else
+            {
+                printDocument.DefaultPageSettings.Landscape = false;
+            }
+            img.Dispose();
+            //printDocument.Print();
+        }
+
+        private void printDocument_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            try
+            {
+                System.Drawing.Image img = System.Drawing.Image.FromFile(openFileDialog.FileName);
+                
+                img = ScaleImage(img, e.MarginBounds.Width, e.MarginBounds.Height); //.Size = e.PageBounds.Size;
+                Point loc = new Point(0,0);
+                e.Graphics.DrawImage(img, loc);
+                img.Dispose();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    }
+}
